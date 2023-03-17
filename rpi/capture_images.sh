1d13c6535fe55c3e2c1bf4a7c8c44b7d129f6bb2
rcollins <robertcollins130@gmail.com>
2023-03-17 12:58:58 -0700

    add script for remote video capture

---
+++ rpi/capture_images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# capture string of images from camera
+
+echo "BEGIN capture_movie.sh"
+echo "Copy movie to $1"
+echo "Recording for $2 s"
+
+# gphoto2 --capture-movie 30s --stdout | ffmpeg -i - -y -pix_fmt yuv420p -b:v 4000k -c:v libx264 test.mp4 
+
+pushd $1
+gphoto2 \
+    --set-config viewfinder=1 \
+    --wait-event 1s \
+    --set-config capturetarget='Memory card' \
+    --wait-event 1s \
+    --set-config movierecordtarget=Card \
+    --wait-event "${2}s" \
+    --set-config movierecordtarget=None \
+    --wait-event 1s \
+    --wait-event-and-download 2s \
+
+gphoto2 \
+    -P \
+
+gphoto2 \
+    -D --recurse \
+
+popd
