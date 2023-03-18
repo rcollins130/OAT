0a389595878798e377d01b18436b6102bdc04299
rcollins <robertcollins130@gmail.com>
2023-03-17 18:49:09 -0700

    fix bug

--- rpi/capture_adsb.sh
+++ rpi/capture_adsb.sh
@@ -12,7 +12,7 @@ mkdir -p $1
 
 # timeout $2 /home/rcollins/git/OAT/rpi/dump1090/dump1090 >> "${1}/$(timestamp)"
 oldpid=$(pgrep dump1090)
-if [ -n oldpid ]
+if [ -n $oldpid ]
 then
         kill $oldpid
 fi
