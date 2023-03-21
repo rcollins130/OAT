04c827d1dca32a4450587e6cfe94d48bb1c1b6b3
rcollins <robertcollins130@gmail.com>
2023-03-21 12:55:35 -0700

    update data capture script to ignore already-copied files

--- local/start_data_capture.sh
+++ local/start_data_capture.sh
@@ -7,5 +7,5 @@ echo "rsync to $3"
 
 ssh rcollins@raspberrypi.local "~/git/OAT/rpi/capture_data.sh $1 $2"
 
-rsync -r rcollins@raspberrypi.local:$1/ $3/
+rsync -hvrPt rcollins@raspberrypi.local:$1/ $3/
 
