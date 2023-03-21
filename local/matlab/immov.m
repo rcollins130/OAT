ccd35d6d60b770f3c8e87b18e0d41bf47f9ab2d0
rcollins <robertcollins130@gmail.com>
2023-03-20 23:22:35 -0700

    Updates to immov util

--- local/matlab/immov.m
+++ local/matlab/immov.m
@@ -11,10 +11,15 @@ if isempty(tdim)
     tdim = ndims(A);
 end
 
+gcf; clf;
+C = repmat({':'},1,ndims(A));
+C{tdim} = 1;
+img = imshow(A(C{:}));
 for ii_f=1:size(A,tdim)
     C = repmat({':'},1,ndims(A));
     C{tdim} = ii_f;
-    imshow(A(C{:})); title(sprintf("%d",ii_f));
+    img.set('CData', A(C{:}));
+    title(sprintf("%d",ii_f));
     pause(0.01)
 end
 
